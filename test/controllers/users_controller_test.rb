f72eb7920284429f5d18c5eeaabbc2e0056b3d57
apandey5 <apandey5@ncsu.github.edu>
2015-10-08 00:14:28 -0400

    controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,35 @@ class UsersControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+def setup
+@user = User.new(name: "user1", email: "uesr1@example.com", password: "user1_user1");
+@admin= User.new(name: "admin", email: "admin@admin.com", password: "admin_admin");
+end
+
+test "should get new" do
+get :new
+assert_response :success
+end
+
+
+test "should get index" do
+get :index
+assert_response :success
+end
+
+test "show admin when logged is an admin" do
+    @admin.save
+    log_in(@admin,is_admin)
+    get :show, id: @admin
+    assert_response :success
+end
+
+test "should allow admin to see all the users" do
+@admin.save
+log_in(@admin,is_admin)
+get:edit, id:@admin
+asser_response:success
+end
+
 end
